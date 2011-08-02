b1106dcd809108ed6ee0b105ce1fbf165b9a2b6a
Ignas Kaziukenas <the.qwer@gmail.com>
2011-08-02 16:08:51 +0300

    Added getSignedUrl

--- API.java
+++ API.java
@@ -2,15 +2,27 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 public class API {
-	public String getKey(String secretAccessKey, String data) {
-		String mykey = secretAccessKey;
-		String test = data;
+	private String key;
+	private String secret;
+	private boolean debug = false;
+	
+	private String getSignedUrl(String url) {
+		if (url.contains("?")) {
+			url += "&key=" + key;
+		} else {
+			url += "?key=" + key;
+		}
+		url += "&signature=" + getSignature(url);
+		return url;
+	}
+	
+	private String getSignature(String data) {
 		try {
 			Mac mac = Mac.getInstance("HmacSHA1");
-			SecretKeySpec secret = new SecretKeySpec(mykey.getBytes(),
+			SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(),
 					"HmacSHA1");
-			mac.init(secret);
-			byte[] digest = mac.doFinal(test.getBytes());
+			mac.init(secretKey);
+			byte[] digest = mac.doFinal(data.getBytes());
 			String key = new String();
 			for (byte b : digest) {
 				key += String.format("%02x", b);
